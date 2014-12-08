b86ff1ad47db050050129db42c1de91fdd69e615
Michael J. Bennett <dev@michaeljbennett.info>
2014-12-07 21:21:54 -0500

    Bootstrapped
     On branch master
    	modified:   .idea/runConfigurations/karma_conf_js.xml
    	modified:   spec/vida-spec.js
    	modified:   src/css/vida.css
    	modified:   src/scss/vida.scss
    	modified:   src/vida.js

--- spec/vida-spec.js
+++ spec/vida-spec.js
@@ -18,4 +18,46 @@ describe('Renderer Service', function () {
     it('should provide a y accessor', inject(function (renderer) {
         expect(typeof renderer.y).toBe('function');
     }));
+
+    it('x should set x if it is a number greater than zero',
+       inject(function (renderer) {
+           renderer.x(65);
+           expect(renderer.x()).toBe(65);
+       }));
+
+    it('y should set y if it is a number greater than zero',
+       inject(function (renderer) {
+           renderer.y(65);
+           expect(renderer.y()).toBe(65);
+       }));
+
+    it('size should set x/y if they are numbers greater than zero',
+       inject(function (renderer) {
+           renderer.size(1, 1);
+           expect(renderer.x()).toBe(1);
+           expect(renderer.y()).toBe(1);
+       }));
+
+    it('size should set x/y if they are numbers greater than zero, including' +
+       'string casts',
+       inject(function (renderer) {
+           renderer.size('10', '11');
+           expect(renderer.x()).toBe(10);
+           expect(renderer.y()).toBe(11);
+       }));
+
+    it('size should return a has with width/height', inject(function (renderer) {
+        expect(renderer.size()).toBeTruthy();
+        expect(typeof renderer.size()).toBe('object');
+        expect(typeof renderer.size().width).toBe('number');
+        expect(typeof renderer.size().height).toBe('number');
+    }));
+
+    it('size should return a has with width/height on invalid inputs',
+       inject(function (renderer) {
+           expect(renderer.size()).toBeTruthy();
+           expect(typeof renderer.size(-253)).toBe('object');
+           expect(typeof renderer.size({}).width).toBe('number');
+           expect(typeof renderer.size(-2352, 235).height).toBe('number');
+       }));
 });
--- src/vida.js
+++ src/vida.js
@@ -15,18 +15,22 @@ angular.module('JSVida', [
     }
 
     return $window.THREE;
-}]).service('renderer', ['three', function (three) {
+}]).factory('_renderer', ['three', function (three) {
     'use strict';
 
-    var renderer = new three.WebGLRenderer(),
+    return new three.WebGLRenderer();
+}]).service('renderer', ['_renderer', function (_renderer) {
+    'use strict';
+
+    var renderer = _renderer,
         /** @type number */
-        x,
+        x = 0,
         /** @type number */
-        y;
+        y = 0;
 
     /**
-     * @param newX {number}
-     * @param newY {number}
+     * @param newX {number=}
+     * @param newY {number=}
      * @returns {{ width: number, height: number}}
      */
     function size(newX, newY) {
@@ -39,11 +43,13 @@ angular.module('JSVida', [
         x = +newX;
         y = +newY;
 
+        renderer.setSize(x, y);
+
         return { width: x, height: y};
     }
 
     /**
-     * @param newX {number}
+     * @param newX {number=}
      * @returns {number}
      */
     function accessX(newX) {
@@ -51,11 +57,12 @@ angular.module('JSVida', [
             return x;
         }
         x = +newX;
+        renderer.setSize(x, y);
         return x;
     }
 
     /**
-     * @param newY {number}
+     * @param newY {number=}
      * @returns {number}
      */
     function accessY(newY) {
@@ -63,9 +70,17 @@ angular.module('JSVida', [
             return y;
         }
         y = +newY;
+        renderer.setSize(x, y);
         return y;
     }
 
+    function start(scene, camera, onFrame) {
+        requestAnimationFrame(function reStart() { start(scene, camera, onFrame); });
+        renderer.render(scene, camera)
+        onFrame();
+    }
+
+    this.start = start;
     this.size = size;
     this.x = accessX;
     this.y = accessY;
@@ -81,25 +96,60 @@ angular.module('JSVida', [
         replace: true,
         template: '<div class="vida-main"><vida-menu></vida-menu><vida-view></vida-view></div>'
     };
-}]).directive('vidaView', ['renderer', function (renderer) {
-    var lastStyle
+}]).directive('vidaView', ['renderer', 'three', function (renderer, three) {
+    var lastStyle,
+        /** @const */
+        shrinkConstant = 25,
+        /** @const */
+        shrinkFactor = 1;
 
     function size(el) {
         lastStyle = getComputedStyle(el[0]);
 
-        renderer.width = lastStyle.width;
-        renderer.height = lastStyle.height;
+        if (!lastStyle) {
+            /** @todo clean up*/
+            console.error('Fatal Error: could not compute viewport size');
+            return;
+        }
+
+        var x = lastStyle.width.substring(0, lastStyle.width.length - 2),
+        y = lastStyle.height.substring(0, lastStyle.height.length - 2);
+
+        x -= shrinkConstant;
+        y -= shrinkConstant;
+
+        x = x/shrinkFactor;
+        y = y/shrinkFactor;
+
+        renderer.size(x, y);
     }
 
     function linkVidaView(scope, elem) {
         size(elem);
 
+        var camera = new three.PerspectiveCamera( 75, renderer.x() / renderer.y(), 0.1, 1000),
+        scene = new three.Scene(),
+        geometry = new three.BoxGeometry(1, 1, 1),
+        material = new three.MeshBasicMaterial({ color: 0x00ff00}),
+        cube = new three.Mesh(geometry, material);
+
+        scene.add(cube);
+        camera.position.z = 5;
+
+        renderer.start(scene, camera, function () {
+            cube.rotation.x += 0.1;
+            cube.rotation.y += 0.01;
+            cube.rotation.z += 0.001;
+        });
+
+
+        angular.element(elem).append(renderer.renderer.domElement);
     }
 
     return {
         restrict: 'E',
         replace: true,
-        link: linkVidaView(),
+        link: linkVidaView,
         template: '<div class="vida-view"></div>'
     }
 }]);
