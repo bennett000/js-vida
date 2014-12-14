18b578d4c483541ac7c36f636c2423966be96aee
Michael J. Bennett <dev@michaeljbennett.info>
2014-12-14 17:37:34 -0500

    Added licensing info
     On branch master
    	new file:   COPYING
    	new file:   COPYING.LESSER
    	modified:   README.md
    	new file:   TODO.md
    	modified:   src/css/vida.css
    	modified:   src/js/3d-objects.js
    	modified:   src/js/3d-render.js
    	modified:   src/js/menu.js
    	modified:   src/js/org-procomyte.js
    	deleted:    src/scss../css/vida.css
    	modified:   src/scss/vida.scss
    	modified:   src/vida.html
    	modified:   src/vida.js
    	new file:   tools/templates/gpl-v3-header
    	new file:   tools/templates/lgpl-header

--- src/js/3d-render.js
+++ src/js/3d-render.js
@@ -1,6 +1,25 @@
 /**
  * file: 3d-render
  * Created by michael on 14/12/14.
+ *
+ @license
+ Vida - Conway inspired life game
+ Copyright © 2014 Michael Bennett
+
+ This file is part of Vida.
+
+ Vida is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ Vida is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with Vida.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 /*global angular */
