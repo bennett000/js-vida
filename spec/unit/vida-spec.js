9b153740fc9d07eca3d3f168a8f9eacd358f7b56
Michael J. Bennett <dev@michaeljbennett.info>
2014-12-15 02:17:31 -0500

    Added license to spec, and gruntfile
     On branch develop
    	modified:   Gruntfile.coffee
    	modified:   spec/unit/vida-spec.js

--- spec/unit/vida-spec.js
+++ spec/unit/vida-spec.js
@@ -1,4 +1,23 @@
-/*global describe, it, spyOn, expect, window, angular, module, inject */
+/**
+ Vida - Conway inspired life game
+ Copyright © 2014 Michael Bennett
+
+ This file is part of Vida.
+
+ Vida is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ Vida is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with Vida.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/*global describe, it, spyOn, expect, window, angular, module, inject, beforeEach */
 
 describe('Renderer Service', function () {
     'use strict';
