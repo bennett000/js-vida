f7d06646993503dd9ca72365af268de16e8851d0
Michael J. Bennett <dev@michaeljbennett.info>
2014-12-14 21:57:06 -0500

    Added simple bootstrap/manifest to start the "build ball" rolling
     On branch develop
    	new file:   src/js/config-physi.js
    	new file:   src/manifest.js
    	modified:   src/vida.html

---
+++ src/js/config-physi.js
@@ -0,0 +1,8 @@
+/**
+ * file: config-physi.js
+ * Created by michael on 14/12/14.
+ */
+
+/*global Physijs*/
+Physijs.scripts.worker = '/lib/physijs/physijs_worker.js';
+Physijs.scripts.ammo = '/lib/ammo.js/builds/ammo.js';
---
+++ src/manifest.js
@@ -0,0 +1,17 @@
+var BOOTSTRAP = Object.freeze({
+  lib: [
+    '/lib/three.js/three.js',
+    '/js/lib-track-control.js',
+    '/lib/physijs/physi.js',
+    '/js/config-physi.js',
+    '/lib/underscore/underscore.js',
+    '/lib/angular/angular.js'
+  ],
+  src: [
+    '/vida.js',
+    '/js/menu.js',
+    '/js/3d-objects.js',
+    '/js/3d-render.js',
+    '/js/org-procomyte.js'
+  ]
+});
