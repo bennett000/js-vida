033248a1efc274112ee249d699058c6eea0c489a
Michael J. Bennett <dev@michaeljbennett.info>
2014-12-26 09:04:53 -0500

    Stashing
     On branch planet
    	modified:   src/vida.js

--- src/vida.js
+++ src/vida.js
@@ -157,7 +157,11 @@ angular.module('JSVida', [
 
     var popMin = 2,
         popMax = 3,
-        tickInterval = 150;
+        tickInterval = 150,
+        colours = {
+            life: {r: 0, g: 200, b: 0},
+            death: {r:0, g: 0, b: 200}
+        };
 
     /**
      * @param seed {Array.<Number>}
@@ -182,23 +186,52 @@ angular.module('JSVida', [
      * @param seed {Array.<Number>}
      * @param x {number}
      * @param y {number}
-     * @returns {Array}
+     * @param pixelMode {number}
+     * @returns {Unit8Array}
      */
-    function completeSeed(seed, x, y) {
+    function completeSeed(seed, x, y, pixelMode) {
+        if ((+pixelMode === 1) || (+pixelMode === 3) || (+pixelMode === 4)) {
+            pixelMode = +pixelMode;
+        } else {
+            pixelMode = 1;
+        }
         x = +x || 0;
         y = +y || 0;
         seed = validateSeed(seed, x, y);
-        var i;
+        var newSeed = new Uint8Array(x * y * pixelMode),
+            colour,
+            i;
 
         for (i = 0; i < (x * y); i += 1) {
-            if (seed[i]) {
-                seed[i] = 1;
-            } else {
-                seed[i] = 0;
+            if (pixelMode === 1) {
+                if (seed[i]) {
+                    newSeed[i] = 1;
+                } else {
+                    newSeed[i] = 0;
+                }
+            } else if (pixelMode === 3) {
+                if (seed[i]) {
+                    colour = colours.life;
+                } else {
+                    colour = colours.death;
+                }
+                newSeed[i * pixelMode] = colour.r;
+                newSeed[i * pixelMode + 1] = colour.g;
+                newSeed[i * pixelMode + 2] = colour.b;
+            } else if (pixelMode === 4) {
+                if (seed[i]) {
+                    colour = colours.life;
+                } else {
+                    colour = colours.death;
+                }
+                newSeed[i * pixelMode] = colour.r;
+                newSeed[i * pixelMode + 1] = colour.g;
+                newSeed[i * pixelMode + 2] = colour.b;
+                newSeed[i * pixelMode + 3] = colour.a;
             }
         }
 
-        return seed;
+        return newSeed;
     }
 
     ///**
@@ -309,43 +342,92 @@ angular.module('JSVida', [
     /**
      * @param seed {Array.<number>}
      * @param result {Array.<number>}
-     * @param x {number}
-     * @param y {number}
+     * @param x {number} limit
+     * @param y {number} limit
      */
-    function tick(seed, result, x, y) {
-        /** @type {number} */
-        var neighbours;
-
+    function brute(seed, result, x, y) {
         seed.forEach(function (cell, offset) {
-            neighbours = liveNeighbours(seed, offset, x, y);
-            // alive cases
-            if (cell === 1) {
-                // under pop case
-                if (neighbours < popMin) {
-                    result[offset] = 0;
-                    return 0;
-                }
-                // even pop case
-                if (neighbours <= popMax) {
-                    result[offset] = 1;
-                    return 1;
-                }
-                // over pop case
+            processCell(seed, result, offset, x, y);
+        });
+
+    }
+
+    /**
+     * @param seed {Array.<number>}
+     * @param result {Array.<number>}
+     * @param offset {number}
+     * @param x {number} limit
+     * @param y {number} limit
+     */
+    function processCell(seed, result, offset, x, y) {
+        /** @type {number} */
+        var neighbours = liveNeighbours(seed, offset, x, y),
+        cell = seed[offset];
+        // alive cases
+        if (cell === 1) {
+            // under pop case
+            if (neighbours < popMin) {
                 result[offset] = 0;
                 return 0;
             }
-            // dead cases
-
-            // birth case
-            if (neighbours === 3) {
+            // even pop case
+            if (neighbours <= popMax) {
                 result[offset] = 1;
                 return 1;
             }
-            // dead is dead case
+            // over pop case
             result[offset] = 0;
             return 0;
-        });
+        }
+        // dead cases
 
+        // birth case
+        if (neighbours === 3) {
+            result[offset] = 1;
+            return 1;
+        }
+        // dead is dead case
+        result[offset] = 0;
+        return 0;
+    }
+
+    /**
+     * @param living {Array.<number>}
+     * @param birthing {Array.<number>}
+     * @param seed {Array.<number>}
+     * @param seed {Array.<number>}
+     * @param x {number}
+     * @param y {number}
+     */
+    function lifeScan(living, birthing, seed, result, x, y) {
+        var offset, i, j;
+        living.forEach(function (cell, i) {
+            // skip garbage
+            if (cell === null) { return; }
+
+            // process main cell, which must be alive, but it can die
+            if (!processCell(seed, result, cell, x, y)) {
+                living[i] = null;
+            }
+
+            // process the neighbours
+            for (i = -1; i < 2; i += 1) {
+                for (j = -1; j < 2; j += 1) {
+                    // skip origin
+                    if ((i === 0) && (j === 0)) {
+                        continue;
+                    }
+                    // process a neighbour
+                    offset = getNeighbour(cell, i, j, x, y);
+                    if ((!seed[offset]) && (birthing.indexOf(offset) === -1)) {
+                        // if a birth happens, register it
+                        if (processCell(seed, result, cell, x, y)) {
+                            birthing.push(offset);
+                        }
+                    }
+                }
+            }
+        });
     }
 
     function Conway(x, y, seed) {
@@ -353,8 +435,16 @@ angular.module('JSVida', [
             return new Conway(x, y, seed);
         }
         var that = makeListener(this),
+            /** @const */
+            pixelMode = 3,
+            /** @bconst */
+            bufferLength = x * y * pixelMode,
             /** @type {Array.<number>} */
-            buffer = [],
+            buffer = new Uint8Array(bufferLength),
+            /** @type {Array.<number>} */
+            livingList = [],
+            /** @type {Array.<number>} */
+            birthingList = [],
             /** @type {boolean} */
             doStop = false,
             /** @type {boolean} */
@@ -364,6 +454,10 @@ angular.module('JSVida', [
 
         completeSeed(seed, x, y);
 
+        livingList = Object.keys(seed).filter(function (el) {
+            return +seed[el] === 1;
+        });
+
         function onTick() {
             if (doStop) {
                 doStop = false;
@@ -372,14 +466,20 @@ angular.module('JSVida', [
                 return;
             }
             if (didFlip) {
-                tick(buffer, seed, x, y);
+                brute(buffer, seed, x, y);
+                //lifeScan(livingList, birthingList, buffer, seed, x, y);
                 that.triggerSync('tick', seed);
                 didFlip = false;
             } else {
-                tick(seed, buffer, x, y);
+                brute(seed, buffer, x, y);
+                //lifeScan(livingList, birthingList, buffer, seed, x, y);
                 that.triggerSync('tick', buffer);
                 didFlip = true;
             }
+            //livingList = livingList.concat(birthingList).filter(function (el) {
+            //    return el !== null;
+            //});
+            //birthingList = [];
             $timeout(onTick, tickInterval);
         }
 
@@ -403,7 +503,7 @@ angular.module('JSVida', [
 }]).factory('planetConway', ['textureSphere', 'three', 'between', 'Conway', function (textureSphere, three, between, Conway) {
     'use strict';
 
-    var res = 256,
+    var res = 512,
         map = new Uint8Array(3 * res * res),
         c = new Conway(res, res, getRandomArray(res * res)),
         material = {
@@ -449,7 +549,13 @@ angular.module('JSVida', [
         material.material.needsUpdate = true;
     }
 
-    c.on('tick', onLifeUpdate);
+    c.on('tick', function (map) {
+        var start = Date.now(),
+            end;
+        onLifeUpdate(map);
+        end = Date.now();
+        console.log('well', (+end - +start), 'ms');
+    });
     c.start();
 
     //function getMap() {
